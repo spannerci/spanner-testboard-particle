6ae95c89d03ff9c6dad8714b2f95884d167f3ec9
Costas Pipilas <cpipilas@econais.com>
2019-05-07 15:51:09 +0300

    update readme, add provisioning script

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# spanner-testboard-particle
-Setup a Particle device as a Spanner Testboard
+# About
+This repository contains resources to setup a Particle device as a Spanner Testboard.
+
+## Supported Particle Devices
+* Argon
+
+## Supported Operating Systems
+* Linux Ubuntu
+* MacOS 10.10 and later
+* Microsoft Windows 7 and later
+
+For Linux and Mac, all the required software will be installed by the provided
+'spannerize.sh' shell script. 
+
+For Microsoft Windows, two packages need to be installed:
+* MinGW (http://www.mingw.org)
+* The Particle CLI setup: https://binaries.particle.io/cli/installer/windows/ParticleCLISetup.exe
+
+For Microsoft Windows 7, extra device drivers needed for Gen 3 devices (argon, xenon, boron):
+* Download Zadig (https://zadig.akeo.ie)
+* Put the Particle device in listening mode (1)
+* Run Zadic app, select “USB Serial (CDC)” and install driver
+* Put the Particle device in DFU Mode (1), select “libusbK(v3.0.7.0)” and install driver
+
+(1) For Particle device modes, check: https://docs.particle.io/tutorials/device-os/led/argon/#listening-mode
+
+## Provisioning
+Provisioning will enable a Particle device to be used as a Spanner Testboard.
+For easy setup, the 'spannerize.sh' provisioning script is provided, just execute:
+
+'''./spannerize.sh'''
+
+and follow the instructions. If troubleshooting, all the particle cli commands 
+included in the script can be called directly from the shell, 
+e.g '''particle identify''' to get the device id.
+
+The provided device id can be used to add the device as a new Testboard in the
+Spanner Web UI.
---
+++ spannerize.sh
@@ -0,0 +1,113 @@
+#!/bin/bash
+
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+CYAN='\033[1;36m'
+NOCOL='\033[0m' # reset color
+
+echo -e "${CYAN}  _____ ____   ____  ____   ____     ___  ____                  
+ / ___/|    \ /    ||    \ |    \   /  _]|    \                 
+(   \_ |  o  )  o  ||  _  ||  _  | /  [_ |  D  )                
+ \__  ||   _/|     ||  |  ||  |  ||    _]|    /                 
+ /  \ ||  |  |  _  ||  |  ||  |  ||   [_ |    \                 
+ \    ||  |  |  |  ||  |  ||  |  ||     ||  .  \                
+  \___||__|  |__|__||__|__||__|__||_____||__|\_|                
+                                                                
+ ______    ___  _____ ______  ____    ___    ____  ____   ___   
+|      |  /  _]/ ___/|      ||    \  /   \  /    ||    \ |   \  
+|      | /  [_(   \_ |      ||  o  )|     ||  o  ||  D  )|    \ 
+|_|  |_||    _]\__  ||_|  |_||     ||  O  ||     ||    / |  D  |
+  |  |  |   [_ /  \ |  |  |  |  O  ||     ||  _  ||    \ |     |
+  |  |  |     |\    |  |  |  |     ||     ||  |  ||  .  \|     |
+  |__|  |_____| \___|  |__|  |_____| \___/ |__|__||__|\_||_____|
+                                                                ${NOCOL}"
+
+echo -e "\n${CYAN}Welcome to the Spanner Testboard configuration!\n
+This script will help you to convert an off-the-shelf Particle device
+in a Spanner Testboard, ready to be added in the Spanner CI platform.${NOCOL}\n"
+
+read -n 1 -s -r -p "Press any key to continue"
+
+if [[ "$OSTYPE" != "msys" ]]; then
+    echo -e "\n--------> Installing particle cli..."
+    bash <( curl -sL https://particle.io/install-cli > /dev/null 2>&1)
+    if [ $? -ne 0 ]; then
+        echo -e "${RED}particle cli installation failed. Aborting...${NOCOL}"
+        exit 1
+    fi
+    sleep 3
+
+    echo -e "\n${CYAN}All required packages installed successfully!${NOCOL}"
+    sleep 3
+else
+    echo -e "\n{YELLOW}Windows OS detected, please make sure to install the
+    particle cli first. The installer can be found here:${NOCOL}
+    https://binaries.particle.io/cli/installer/windows/ParticleCLISetup.exe\n"
+
+    read -n 1 -s -r -p "If the particle cli is installed, press any key to continue"
+fi
+
+echo -e "\n\n${YELLOW}PLEASE PUT THE DEVICE IN DFU MODE${NOCOL}
+To enter DFU Mode:
+1. Hold down BOTH buttons
+2. Release only the RESET button, while holding down the SETUP button
+3. Wait for the LED to start flashing yellow (it will flash magenta first)
+4. Release the SETUP button\n"
+
+read -p "If the device is in DFU mode, press ENTER to continue"
+
+echo -e "\n--------> Flashing core Testboard firmware..."
+particle flash --usb hybrid-0.9.0-argon.bin
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Failed to flash core firmware! Aborting...${NOCOL}"
+    exit 1
+fi
+sleep 3
+
+echo -e "\n--------> Flashing Testboard firmware app..."
+particle flash --usb spanner_firmware.bin
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Failed to flash app firmware! Aborting...${NOCOL}"
+    exit 1
+fi
+sleep 3
+
+# Linux ModemManager thinks any new ttyACM device is a modem - also known as
+# the ttyACM0 drama!
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    sudo systemctl stop ModemManager.service
+fi
+
+echo -e "\n--------> Setting Wi-Fi Credentials"
+particle serial wifi
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Failed to set wifi creds! Aborting...${NOCOL}"
+    exit 1
+fi
+sleep 3
+
+echo -e "\n"
+read -p "Please press the reset button in the device and then press ENTER to continue"
+echo -e "\n--------> Getting Device ID..."
+sleep 3
+particle identify
+if [ $? -ne 0 ]; then
+    echo -e "${RED}Failed to get device id! Aborting...${NOCOL}"
+    exit 1
+fi
+sleep 3
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+    sudo systemctl start ModemManager.service
+fi
+
+echo -e "\n${CYAN}
+Great Job, your new Spanner Testboard is ready! 
+Now you can add it as a new Testboard in the Spanner CI platform by using
+the above device id.${NOCOL}"
+
+echo -e "${YELLOW}
+Note: if the device is flashing blue, press the MODE button for ~8 seconds.
+As soon as you release it, it will start flashing green and the device will 
+try to connect to the network. A steady cyan color means that the device is 
+connected and ready to be used.${NOCOL}\n"
